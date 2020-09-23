565901488387eac823feecf2c6dcf3cfa62d1fb1
woodchucker <faditra@inwind.it>
2020-09-23 18:48:48 +0200

    _

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions Options = new ChromeOptions();
+            List<string> ls = new List<string>();
+            ls.Add("enable-automation");
+            Options.AddArguments("--disable-notifications");
+            Options.AddUserProfilePreference("intl.accept_languages", "en-US");
+            Options.AddExcludedArguments(ls);
+            IWebDriver Driver = new ChromeDriver(Options);
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Learn Automation"+ Keys.Return);            
+        }
+    }
+}
